b72a1ad3d133a78a295c38feb3179eb255a15e9d
Dylan  Lott <lott.dylan@gmail.com>
2017-06-16 06:57:00 -0600

    Add test and setup initial schema plugin

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 "use strict";
 
 module.exports = function (schema, options) {
+  console.log('schema registered: ', schema);
+  console.log('options: ', options);
 
+  schema.pre('save', function(next) {
+    console.log('pre schema save', next);
+    next();
+  });
 }
